6a9f7c895b944998cccf413263309c0aa9df6a85
Collin OConnell <ococncol@gmail.com>
2020-07-08 10:15:41 -0400

    almost done

---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <Logo />
+    <h1 class="title">nuxtschool</h1>
+    <div class="links">
+      <nuxt-link
+        v-for="post in posts"
+        :to="{ name: 'posts-id', params: { id: post.id } }"
+        :key="post.id"
+      >{{ post.title }}</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "Home page",
+      meta: [
+        { name: "twitter:title", content: "Nuxt Fundementals by Vue School" },
+        { name: "twitter:content", content: "Nuxt + Vue" },
+        { name: "twitter:image", content: "https://i.imgur.com/UYP2umJ.png" },
+        { name: "twitter:card", content: "summary_large_image" }
+      ]
+    };
+  },
+  computed: {
+    posts() {
+      return this.$store.state.posts.all;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
