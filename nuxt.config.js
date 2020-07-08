7bce84632a3d35cc3acbed92874560a7c7cde786
Collin OConnell <ococncol@gmail.com>
2020-07-08 10:53:04 -0400

    looks to be working now

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,9 +17,9 @@ export default {
   head: {
     title: process.env.npm_package_name || '',
     htmlAttrs: {
-      lang: 'en',
+      lang: "en"
     },
-    meta: [,
+    meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
