72fbd9a14a21cc80e1640733bf401bdf745796a3
Collin OConnell <ococncol@gmail.com>
2020-07-08 10:41:27 -0400

    added lang

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,8 +16,10 @@ export default {
   */
   head: {
     title: process.env.npm_package_name || '',
-    meta: [
-      { lang: 'en' },
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [,
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
